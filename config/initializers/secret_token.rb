d383c5ad280096e943d8bf2fa04fe39a1ac239d6
Cole Pilegard <c.pilegard@gmail.com>
2013-08-07 13:18:04 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GeoVerflow::Application.config.secret_token = '123f2f3b63ef350fa2e3c85f198da195b8337649d33445b8e6f4a22abf87b0b60687edfc7e7de7651f3ce011c9340d53e63f92b9804603d81d7d5ea01a8ebbc8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+GeoVerflow::Application.config.session_store :cookie_store, key: '_geo-verflow_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# GeoVerflow::Application.config.session_store :active_record_store
