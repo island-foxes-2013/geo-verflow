e1e8ac071fd67902cdda17207a0fd3636725205e
Cole Pilegard <c.pilegard@gmail.com>
2013-08-07 16:26:07 -0700

    tests for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
   attr_accessible :password, :username
+  validates :username, :password, presence: true
+  validates_uniqueness_of :username
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,33 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  let(:user) { User.new }
+  let(:user2) { User.new }
+  describe "#initialize" do
+
+    context "happy data" do
+      it "should save to the database" do
+        user.username = "someuser"
+        user.password = "password"
+        user.save.should eq true
+      end
+    end
+
+    context "bad data" do
+      it "should not save with empty data" do
+        user.save.should eq false
+      end
+
+      it "should not save with a non-unique username" do
+        user.username = "someuser"
+        user.password = "password"
+        user.save
+
+        user2.username = "someuser"
+        user2.password = "password"
+        user2.save.should eq false
+      end
+    end
+  end
 end
