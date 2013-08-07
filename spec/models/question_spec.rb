c771687cc9cc5a6d914ccdd0b30573f867596479
adamloo85 <adamloo85@gmail.com>
2013-08-07 14:53:20 -0700

    created question instance test

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
 describe Question do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:question) { Question.new(title: "test", content: "test_content", upvotes: 0, downvotes: 0) }
+
+  it "is an object of Question" do
+    expect(question).to be_an_instance_of Question
+  end
 end
