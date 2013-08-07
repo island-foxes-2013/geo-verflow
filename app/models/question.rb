f8eebabc282c34df0cadbe4f18d6ae1d9d1d4b04
adamloo85 <adamloo85@gmail.com>
2013-08-07 14:57:20 -0700

    first test in question class

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,3 @@
 class Question < ActiveRecord::Base
-  attr_accessible :title, :content, :upvotes, :downvotes, 
+  attr_accessible :title, :content, :upvotes, :downvotes
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -4,6 +4,6 @@ describe Question do
   let(:question) { Question.new(title: "test", content: "test_content", upvotes: 0, downvotes: 0) }
 
   it "is an object of Question" do
-    expect(question).to be_an_instance_of Question
+    expect(question.class).to be Question
   end
 end
