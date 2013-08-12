6172ebfb825bf83c1eb0433e23aadd4120f5412d
Jeffrey Wescott <jeffrey.wescott@gmail.com>
2013-08-12 11:43:35 -0700

    DRY-up current_user helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-
-  def current_user
-    if session[:user]
-      @current_user = User.find(session[:user].to_i)
-    else
-      nil
-    end
-  end
+  include ApplicationHelper
 end
